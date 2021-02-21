a161f50235f01b6d16ab85b7a1a1fc30fd529d47
Jandir A. Cutabiala <jandir.dev@gmail.com>
2021-02-21 04:25:42 -0300

    chore: set new variable

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
     return (
         <div>
-            <h1>Hi, there! I'm working</h1>
+            <h1>Hi, there!</h1>
             <hr />
 
             <MarketingApp />
