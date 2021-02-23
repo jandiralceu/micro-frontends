be97a387bfcee73dffc0bcfe2c650b970a6f6ac4
Jandir A. Cutabiala <jandir.dev@gmail.com>
2021-02-23 02:47:10 -0300

    chore: prefix class names on production

--- container/src/App.js
+++ container/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ctr'
+})
+
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <h1>Hi, there!</h1>
-                <hr />
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <h1>Hi, there!</h1>
+                    <hr />
 
-                <MarketingApp />
-            </div>
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
